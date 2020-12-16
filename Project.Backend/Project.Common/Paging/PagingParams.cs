52b0005cd80a4d584db6e4b88d9582f9fa403c90
andjelo <andbaric@gmail.com>
2020-12-16 05:28:32 +0100

    fix read makes misspelled

---
+++ Project.Backend/Project.Common/Paging/PagingParams.cs
@@ -0,0 +1,8 @@
+﻿namespace Project.Common.Paging
+{
+    public class PagingParams
+    {
+        public int PageSize { get; set; } = 1;
+        public int PageNumber { get; set; } = 10;
+    }
+}
